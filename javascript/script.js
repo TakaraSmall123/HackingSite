98dfc8e69b01b0754aae1991cac5cd43ce4bd742
Takara Small <a.simpsonlately@gmail.com>
2017-04-04 17:22:40 -0400

    Basic outline w/ notes included

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,12 @@
 
 //PWNCOUNT API STEPS
 
-// create name spacing
 var hackApp = {}
 
-// create init function
 hackApp.init = function(){
 	hackApp.getHackInfo();
 }
 
-//make request
 hackApp.getHackInfo = function(query) {
 	$.ajax({
 		url: 'https://haveibeenpwned.com/api/v2/breaches',
@@ -25,29 +22,19 @@ hackApp.getHackInfo = function(query) {
 	})
 };
 
-
-//put the variable from getHackInfo here
 hackApp.displayInfo = function(items) {
-	// $('#hackWork').empty();
-	// console.log('testing2',items)
 	items.forEach(function(item){
-		//loop over every item returned from API
 		if(item.IsVerified === true && item.AddedDate >= "2015-01-01" && item.PwnCount >= "1500000" || item.Title === "Yahoo") {
 			var website = item.Title;
 			var name = item.Description;
 			var dataClasses = item.DataClasses;
-			//add outline for adding it to index
 			var nameElement = $('<button class="newsButton" id="siteNames">').text(website);
 			var websiteElement = $('<p></p>').html(`<br><div class="url"><i class="fa fa-book" aria-hidden="true"></i></div><strong>Background Information:</strong> ` + name);
-			// $('<div class="infoContainer></div>').html(items).append('.container')
 			var dataElement = $('<p></p>').html("<br></br><strong>Stolen info:</strong> " + dataClasses + " <br></br>");
-			// var dataElement = $('<p class="dataAttr">').html("<strong>Stolen info:</strong> " + dataClasses + "").hide();
-			//create variable for what will be on page so add it later
 			var pwnedElement = $('<div class="pwnedDiv"></div>').append(websiteElement, dataElement).hide();
 			var nytElement = $('<div class="nytContainer"></div>').hide();
 			var titleSanitized = item.Title.replace(/\s/g, '');
 			var hackSheet = $('<div>').addClass('item').addClass('clearfix').addClass(titleSanitized).append(nameElement,nytElement, pwnedElement);
-			//add to website
 			$('#hackWork').append(hackSheet);
 
 		}
@@ -55,17 +42,11 @@ hackApp.displayInfo = function(items) {
 
 };
 
-//NYT API STEPS
-//create namespacing, nyt key, nyt url variables to reuse
 var timesApp = {}
 timesApp.key = 'b2649c169cdb439e9ebfb626605b499b';
 timesApp.url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-// create a way to insert search params into nyt app https://api.jquery.com/jQuery.param/
 
-//create function to return the information we want from nyt. The "news" parameter will change to pwn selection
 timesApp.getNewsInfo = function(newsList){
-	console.log(newsList);
-
 	$.ajax({
 		url: timesApp.url,
 		method: 'GET',
@@ -80,25 +61,16 @@ timesApp.getNewsInfo = function(newsList){
 		},
 
 		}).then(function(clips){
-			console.log(clips);
-
 			var newsListSanitized = newsList.replace(/\s/g, '');
-		
 			if(clips.response.docs.length < 1 ) {
-
 				$(`.${newsListSanitized} .nytContainer`).html(`<br></br><div class="url"><p><i class="fa fa-hand-paper-o" aria-hidden="true"></i><br><strong>Sorry, there are no current <i>NYT</i> stories that match ${newsListSanitized}.</strong></p>`).css()
 
 			}
 
 			else {
-
-
 			var filteredByAbstract = clips.response.docs.filter(function(item) {
-				console.log(item);
 				return item.abstract !== null;
 			});
-
-			console.log(filteredByAbstract)
 			var addAbstract;
 			if(filteredByAbstract.length > 0) {
 				addAbstract = filteredByAbstract[0].abstract
@@ -111,22 +83,176 @@ timesApp.getNewsInfo = function(newsList){
 			var addLeadParagraph = clips.response.docs[0].lead_paragraph;
 			var addUrl = clips.response.docs[0].web_url;
 			var addSnippet = clips.response.docs[0].snippet;
-			console.log(addHeadline)
 		
 			$('.nytWebsite').append(clips.response.docs[0].web_url);
 			$(`.${newsListSanitized} .nytContainer`).html(`<br><div class="url"><blockquote><p><i class="fa fa-newspaper-o" aria-hidden="true"></i><br><strong>${addHeadline}</strong><br></br></div></p><div class="snip"><p>${addSnippet}</p><a href="${addUrl}">Read the full of NYT story</a></div>`);
-			console.log(addUrl);
 		}
 
 	})
 };
 
-
-
-//Start document ready!
 $(function(){
 
-//init request
+
+
+//LINKS
+$('.intro').on('click', function(){
+	$(this).parents('section').hide()
+	$('header').fadeIn();
+	// $('.introWrapper').show();
+});
+
+$('.stand').on('click', function(){
+	$(this).parents('section').hide()
+	$('.quoteSection').fadeIn();
+	// $('.introWrapper').show();
+});
+
+$('.why').on('click', function(){
+	$(this).parents('section').hide()
+	$('#whyItMattersSection').fadeIn();
+	// $('.introWrapper').show();
+});
+
+$('.how').on('click', function(){
+	$(this).parents('section').hide()
+	$('#howToProtectSection').fadeIn();
+	// $('.introWrapper').show();
+});
+
+$('.hackedInfo').on('click', function(){
+	$(this).parents('section').hide()
+	$('#hackedInfoSection').fadeIn();
+	// $('.introWrapper').show();
+});
+
+///////////////////SEPERATE SECTION
+
+$('#why').on('click', function(){
+	$('.quoteSection').hide();
+	$('.whyItMattersSection').fadeIn();
+});
+
+$('#how').on('click', function(){
+	$('.quoteSection').hide();
+	$('.howToProtectSection').fadeIn();
+});
+
+$('#hackedInfo').on('click', function(){
+	$('.quoteSection').hide();
+	$('.hackWork').fadeIn();
+});
+
+//CHEVRON SECTION
+
+$('#introChevron').on('click', function(){
+	$('header').hide();
+	$('.quoteSection').fadeIn();
+});
+
+$('#quoteChevron').on('click', function(){
+	$('.quoteSection').hide();
+	$('.whyItMattersSection').fadeIn();
+});
+
+$('#whyItMattersChevron').on('click', function(){
+	$('.whyItMattersSection').hide();
+	$('.demoSection').fadeIn();
+});
+
+$('#demoChevron').on('click', function(){
+	$('.demoSection').hide();
+	$('.howToProtectSection').fadeIn();
+});
+
+$('#howToProtectChevron').on('click', function(){
+	$('.howToProtectSection').hide();
+	$('.hackedInfoSection').fadeIn();
+});
+
+//////////////// POPOVER SECTION
+
+$('[data-toggle="popover"]').fadeIn().popover();  
+
+
+///TYPEWRITER SECTION
+
+function setupTypewriter(t) {
+        var HTML = t.innerHTML;
+
+        t.innerHTML = "";
+
+        var cursorPosition = 0,
+            tag = "",
+            writingTag = false,
+            tagOpen = false,
+            typeSpeed = 25,
+        	tempTypeSpeed = 0;
+
+        var type = function() {
+        
+            if (writingTag === true) {
+                tag += HTML[cursorPosition];
+            }
+
+            if (HTML[cursorPosition] === "<") {
+                tempTypeSpeed = 0;
+                if (tagOpen) {
+                    tagOpen = false;
+                    writingTag = true;
+                } else {
+                    tag = "";
+                    tagOpen = true;
+                    writingTag = true;
+                    tag += HTML[cursorPosition];
+                }
+            }
+            if (!writingTag && tagOpen) {
+                tag.innerHTML += HTML[cursorPosition];
+            }
+            if (!writingTag && !tagOpen) {
+                if (HTML[cursorPosition] === " ") {
+                    tempTypeSpeed = 0;
+                }
+                else {
+                    tempTypeSpeed = (Math.random() * typeSpeed) + 40;
+                }
+                t.innerHTML += HTML[cursorPosition];
+            }
+            if (writingTag === true && HTML[cursorPosition] === ">") {
+                tempTypeSpeed = (Math.random() * typeSpeed) + 40;
+                writingTag = false;
+                if (tagOpen) {
+                    var newSpan = document.createElement("span");
+                    t.appendChild(newSpan);
+                    newSpan.innerHTML = tag;
+                    tag = newSpan.firstChild;
+                }
+            }
+
+            cursorPosition += 1;
+            if (cursorPosition < HTML.length - 1) {
+                setTimeout(type, tempTypeSpeed);
+            }
+
+        };
+
+        return {
+            type: type
+        };
+    }
+
+    var typer = document.getElementById('typewriter');
+
+    typewriter = setupTypewriter(typewriter);
+
+    typewriter.type();
+
+    });
+
+
+/////// API SECTION
+
 hackApp.init();
 
 function myFunction() {
@@ -134,9 +260,11 @@ newsList = website;
 newsResults = dataClasses;
 }
 
-//click function
+$('.twitter').on('click', function(){
+$('.twitter').html(`<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Check%20out%20this%20year's%20hacked%20sites%20">Share your fortune on Twitter</a>`)
+})	 
+
 $('#hackWork').on('click', '.newsButton',function(){
-	console.log('yo, this test')
 	var company = $(this).text()
 	timesApp.getNewsInfo(company)
 	$(this).siblings('.nytContainer').slideToggle()
@@ -144,45 +272,37 @@ $('#hackWork').on('click', '.newsButton',function(){
 	$(this).siblings('.pwnedDiv').css("font-size", "2rem");
 });
 
-//blink function
-var questionMark = $(".blink");
-var shown = true;
-setInterval(toggle, 400);
+// var questionMark = $(".blink");
+// var shown = true;
+// setInterval(toggle, 400);
 
-function toggle() {
-	if(shown) {
-    questionMark.hide();
-    shown = false;
-	} else {
-    questionMark.show();
-    shown = true;
-	}
-}
+// function toggle() {
+// 	if(shown) {
+//     questionMark.hide();
+//     shown = false;
+// 	} else {
+//     questionMark.show();
+//     shown = true;
+// 	}
+// }
 
-//quiz button
 $(".startQuiz").on("click", function(){
 $(".standaloneQuote").fadeIn('slow');
 $(".attribution").fadeIn('slow');
 
 });
 
-// scroll button
 $(".scrollOne").on("click", function(){
 $(".newsPargraphs").fadeIn('slow'); 
 
 });
 
-//count function
 var n = 0;
 
 $('#hackWork').on('click', '.newsButton',function(){
-console.log("There's another one")
 $("#hackWork").each(function() {
 var update = $('.pwnedDiv').text()
 n = n + 1;
-console.log("There's two", n);
-console.log("There's two", update);
-
 $(".totalNumbers").text(n);
 
 }); 
@@ -193,16 +313,26 @@ $(".totalNumbers").text(n);
 
 	 });
 
+// $('.share').html(`<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Find%20your%20personal%20data%20online%20here%20http://bit.ly/2mqRq0j">Share your hacking data online</a>`)	
 
-//share button
-$('.share').html(`<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Find%20your%20personal%20data%20online%20here%20http://bit.ly/2mqRq0j">Share your hacking data online</a>`)	
+// });
 
-});
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
  }); 
 
 
-// });
 
 
 
